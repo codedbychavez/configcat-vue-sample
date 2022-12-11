87aac3e3f7d11185fd78b72afd567a0e209d1862
codedbychavez <codedbychavez@gmail.com>
2022-12-11 15:18:43 -0300

    Updated sample app

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="my-app">
     <FeatureWrapper featureKey="featurekey">
       <p>
-        I will show if the feature flag with <b>featurekey</b> is enabled in ConfigCat
+        This will show if the feature flag with <b>featurekey</b> is enabled in ConfigCat
       </p>
     </FeatureWrapper>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 import { ConfigCatPlugin } from 'configcat-vue';
 
 const app = createApp(App);
 
 app.use(ConfigCatPlugin, {
-  apiKey: "ScDaCD8ETUuG7wYo3BdP2A/5s96HBVckk-RzI-iVf-zRA"
+  sdkKey: "YOUR-CONFIGCAT-SDK-KEY"
 })
 
-app.mount('#app')
+app.mount('#app');
