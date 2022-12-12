356ae0d97ad4185e012d78922703d12638ca4da6
codedbychavez <codedbychavez@gmail.com>
2022-12-12 17:34:47 -0300

    Updated sample app

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div class="my-app">
     <FeatureWrapper featureKey="featurekey">
       <p>
-        This will show if the feature flag with <b>featurekey</b> is enabled in ConfigCat
+        This will show if the feature flag with <b>featurekey</b> is enabled in
+        ConfigCat
       </p>
     </FeatureWrapper>
   </div>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,26 @@ import App from './App.vue';
 
 import { ConfigCatPlugin } from 'configcat-vue';
 
+/*
+If you need to create a logger
+
+install configcat-js (npm package) as shown below
+
+documentation: https://configcat.com/docs/sdk-reference/js/#logging
+*/
+
+import * as configcat from 'configcat-js';
+const logger = configcat.createConsoleLogger(3);
+
 const app = createApp(App);
 
 app.use(ConfigCatPlugin, {
-  sdkKey: "YOUR-CONFIGCAT-SDK-KEY"
-})
+  SDKKey: "YOUR-SDK-KEY",
+  // clientOptions is optional
+  clientOptions: {
+    pollIntervalSeconds: 95,
+    logger: logger,
+  }
+});
 
 app.mount('#app');
