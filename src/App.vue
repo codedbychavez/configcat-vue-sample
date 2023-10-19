5716e71d650678d02a74366e55f0c3561e8ef0ec
Chavez Harris <codedbychavez@gmail.com>
2023-10-19 18:38:22 -0300

    Init test

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="my-app">
+  <div class="container">
+    <TheWelcome />
+  </div>
+</div>
+</template>
+
+<script setup lang="ts">
+import TheWelcome from './components/TheWelcome.vue';
+</script>
+
+<style scoped>
+.my-app {
+  display: flex;
+  justify-content: center;
+}
+
+.feature-not-available-wrapper {
+  background-color: orange;
+  padding: 1rem 2rem;
+  border-left: 0.4rem solid orangered;
+}
+</style>
