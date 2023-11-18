b53aedb5b920e0828cfefa3154837700cb4904c8
Chavez Harris <codedbychavez@gmail.com>
2023-11-18 08:15:57 -0400

    Add missing anchor links to resources

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,16 +1,20 @@
 <template>
   <section class="welcome">
     <div class="text-container">
-      <h1 class="title">Welcome to the sample app for the configcat-vue plugin</h1>
+      <h1 class="title">Welcome to the sample app for the <a href="https://www.npmjs.com/package/configcat-vue" target="_blank">configcat-vue</a> plugin 🎉</h1>
       <p class="description">
         This application is kept as basic as possible to help you quickly navigate around and
         explore its source code.
       </p>
       <h2>Instructions for using this sample app</h2>
       <p>
-        You can find the instructions
-        <!-- TODO: Add update href here -->
-        <a href="/" target="_blank">here</a>
+        You can find the <b>instructions for using this sample app
+        <a href="https://github.com/codedbychavez/configcat-vue-sample" target="_blank">here</a></b>
+      </p>
+      <h2>Instructions for using the configcat-vue plugin</h2>
+      <p>
+        You can find the <b>instructions for using the configcat-vue plugin
+        <a href="https://configcat-vue.netlify.app/quick-start.html" target="_blank">here</a></b>
       </p>
       <h2>The feature below</h2>
       <p>If you've setup this sample app correctly, the feature will be displayed below.</p>
